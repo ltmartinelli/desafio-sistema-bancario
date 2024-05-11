7a8985603c927d405e70ecf6a25da28db4bf013a
ltmartinelli <lucastrindademartinelli@gmail.com>
2024-05-10 21:57:06 -0300

    fix

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ menu = """
 
 saldo = 0
 limite = 500.00
-extrato = "\nEXTRATO:\n"
+extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+realizou_movimentacoes = False
 
 while True:
 
@@ -22,6 +23,7 @@ while True:
         if valor > 0:
             saldo += valor
             extrato += f'Depósito: + R$ {valor:.2f}\n'
+            realizou_movimentacoes = True
         else:
             print('Apenas valores positivos!')
 
@@ -32,19 +34,22 @@ while True:
             valor = float(input('Quanto você quer sacar?'))
             if valor > limite:
                 print(f'Valor inválido, o limite de saque é de R$ {limite:.2f}')
-            elif saldo-valor < 0:
+            elif valor > saldo:
                 print(f'Você não possui saldo o suficiente. Seu saldo é de R$ {saldo:.2f}')
             elif valor <= 0:
                 print(f'Valor inválido, o saque deve ser maior que R$ 0.00')
             else:
                 saldo -= valor
-                numero_saques+=1
+                numero_saques += 1
                 extrato += f'Saque: - R$ {valor:.2f}\n'
+                realizou_movimentacoes = True
                 print('Operação concluída!')
 
     elif opcao == 'e':
-        print(extrato)
+        print('\n============EXTRATO=============:\n')
+        print(extrato) if realizou_movimentacoes else print('Não foram realizadas movimentações.')
         print(f'Saldo Final = R$ {saldo:.2f}:')
+        print('\n===============================:\n')
     elif opcao == 'q':
         break
     else:
